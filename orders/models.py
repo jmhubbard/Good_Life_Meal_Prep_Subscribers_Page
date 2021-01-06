2477807311bdb626ac4293450ac0e6745887240f
Jason Hubbard <jasonhubb@gmail.com>
2021-01-06 14:32:36 -0800

    creates order and orderitem models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from recipes.models import Recipe
+from users.models import User
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='order')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Order for {self.user.name} - {self.created_at}'
+
+class OrderItem(models.Model):
+    name = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='order_item')
+    quantity = models.PositiveSmallIntegerField(null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_item')
+    
+
+    def __str__(self):
+        return f'{self.quantity} orders of {self.name}'
