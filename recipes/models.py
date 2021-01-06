4e1932822025a58d1a02cf4857e872f601df0ff3
Jason Hubbard <jasonhubb@gmail.com>
2021-01-05 21:54:53 -0800

    creates Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    proteins = models.PositiveSmallIntegerField(null=True, blank=True)
+    carbs = models.PositiveSmallIntegerField(null=True, blank=True)
+    fats = models.PositiveSmallIntegerField(null=True, blank=True)
+    calories = models.PositiveSmallIntegerField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
