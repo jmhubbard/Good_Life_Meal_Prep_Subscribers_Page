3b432c3bee10fb75298dc78283c880f93db1dd12
Jason Hubbard <jasonhubb@gmail.com>
2021-01-05 20:45:20 -0800

    added name in required fields

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import (
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, date_of_birth, password=None):
+    def create_user(self, email, name, date_of_birth, password=None):
         """
         Creates and saves a User with the given email, date of
         birth and password.
@@ -15,6 +15,7 @@ class UserManager(BaseUserManager):
 
         user = self.model(
             email=self.normalize_email(email),
+            name=name,
             date_of_birth=date_of_birth,
         )
 
@@ -22,13 +23,14 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, date_of_birth, password=None):
+    def create_superuser(self, email, name, date_of_birth, password=None):
         """
         Creates and saves a superuser with the given email, date of
         birth and password.
         """
         user = self.create_user(
             email,
+            name=name,
             password=password,
             date_of_birth=date_of_birth,
         )
@@ -47,11 +49,14 @@ class User(AbstractBaseUser):
     date_of_birth = models.DateField()
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['date_of_birth']
+    REQUIRED_FIELDS = ['name', 'date_of_birth']
 
     def __str__(self):
         return self.email
