507a7086276b75a4b3a50969f84ab79e71eadb8b
Jason Hubbard <jasonhubb@gmail.com>
2021-01-07 15:05:31 -0800

    adds is_active boolean field to Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -10,6 +10,9 @@ class Recipe(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=False)
+
+
 
     def __str__(self):
         return self.name
