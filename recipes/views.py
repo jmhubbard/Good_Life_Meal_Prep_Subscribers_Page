600fc43a30dd99dd946f36cc00a66a6c3c2f00df
Jason Hubbard <jasonhubb@gmail.com>
2021-01-06 20:59:21 -0800

    creates menu view

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Recipe
+
+def menu(request):
+    current_user = request.user
+    recipes = Recipe.objects.all()
+    context ={
+        'current_user': current_user,
+        'recipes': recipes
+    }
+
+    return render(request, 'recipes/menu.html', context)
