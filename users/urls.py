fcf176273634cdf019de74ea21b68a9d3f50f5e5
Jason Hubbard <jasonhubb@gmail.com>
2021-01-06 20:58:54 -0800

    creates logout path

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from main.views import UserLoginView
+from main.views import UserLoginView, UserLogoutView
 
 urlpatterns = [
-    path('login/', UserLoginView.as_view(), name="login")
+    path('login/', UserLoginView.as_view(), name="login"),
+    path('logout/' , UserLogoutView.as_view(), name='logout'),
 ]
