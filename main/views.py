77a691e6248c2d7f259d2e760b35c42a15fb38fd
Jason Hubbard <jasonhubb@gmail.com>
2021-01-06 18:09:09 -0800

    creates login.html and login view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.views import (LoginView,)
+
+class UserLoginView(LoginView):
+
+    template_name = "registration/login.html"
+
+    # @method_decorator(unauthenticated_user) #If user is already authenticated they will be redirected to their subscription page
+    # def dispatch(self, *args, **kwargs):
+    #     return super().dispatch(*args, **kwargs)
