74989dcd5f7f4765c1bf2a17366782410b985448
Jason Hubbard <jasonhubb@gmail.com>
2021-01-06 20:59:15 -0800

    creates a path to view menu view

--- orders/urls.py
+++ orders/urls.py
@@ -3,6 +3,11 @@ from django.urls import include, path
 
 from .views import OrderItemCreateView
 
+
+
+from recipes.views import menu
+
 urlpatterns = [
-    path('', OrderItemCreateView.as_view(), name="order-create"),
+    # path('', OrderItemCreateView.as_view(), name="order-create"),
+    path('', menu, name="menu" )
 ]
