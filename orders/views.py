2b27f1c99cac49e9879f82469fbbc0687054194f
Jason Hubbard <jasonhubb@gmail.com>
2021-01-08 00:17:21 -0800

    deleted orders app

--- orders/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.generic.edit import CreateView
-from .models import Order, OrderItem
-
-class OrderItemCreateView(CreateView):
-    model = OrderItem
-    fields =['name', 'quantity']
