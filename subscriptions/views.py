0839b4e043050da34cfa88fe29e05ed0ba1c53d3
Jason Hubbard <jasonhubb@gmail.com>
2021-01-07 22:08:33 -0800

    creates subscriptionupdate view

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic.edit import UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
+from django.core.exceptions import PermissionDenied
+from django.http import Http404
+
+
+
+
+
 from .models import SubscriptionItem
 
+@login_required()
 def subscriptionView(request):
     current_user = request.user
     subscriptions = SubscriptionItem.objects.filter(user = current_user, item__is_active = True).order_by('item__name')
@@ -10,3 +22,56 @@ def subscriptionView(request):
     }
 
     return render(request, 'subscriptions/subform.html', context)
+
+
+class SubscriptionUpdate(LoginRequiredMixin, UpdateView):
+    model = SubscriptionItem
+    fields = ['quantity',]
+    template_name_suffix = '_update_form'
+    # form_class = SubscriptionForm
+    
+    
+    def get_success_url(self):
+        return reverse('subscriptions')
+
+
+    def get_object(self, queryset=None):
+        """
+        Return the object the view is displaying.
+        Require `self.queryset` and a `pk` or `slug` argument in the URLconf.
+        Subclasses can override this to return any object.
+        """
+        # Use a custom queryset if provided; this is required for subclasses
+        # like DateDetailView
+        if queryset is None:
+            queryset = self.get_queryset()
+
+        # Next, try looking up by primary key.
+        pk = self.kwargs.get(self.pk_url_kwarg)
+        slug = self.kwargs.get(self.slug_url_kwarg)
+        if pk is not None:
+            queryset = queryset.filter(pk=pk)
+        # Next, try looking up by slug.
+        if slug is not None and (pk is None or self.query_pk_and_slug):
+            slug_field = self.get_slug_field()
+            queryset = queryset.filter(**{slug_field: slug})
+        # If none of those are defined, it's an error.
+        if pk is None and slug is None:
+            raise AttributeError(
+                "Generic detail view %s must be called with either an object "
+                "pk or a slug in the URLconf." % self.__class__.__name__
+            )
+        try:
+            # Get the single item from the filtered queryset
+            obj = queryset.get()
+        except queryset.model.DoesNotExist:
+            raise Http404(("No %(verbose_name)s found matching the query") %
+                        {'verbose_name': queryset.model._meta.verbose_name})
+
+        current_user = self.request.user
+
+        if current_user != obj.user:
+            raise PermissionDenied
+        
+        return obj
+
