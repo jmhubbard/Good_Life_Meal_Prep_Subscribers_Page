902866d9ba6e14a2bd0a8cdfc23374d6bab4f8c4
Jason Hubbard <jasonhubb@gmail.com>
2021-01-08 00:59:16 -0800

    deletes subscriptions app

--- subscriptions/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-
-from .views import subscriptionView, SubscriptionUpdate
-
-urlpatterns = [
-    path('', subscriptionView, name="subscriptions" ),
-    path('update/<int:pk>/', SubscriptionUpdate.as_view(), name='subscription-update')
-]
