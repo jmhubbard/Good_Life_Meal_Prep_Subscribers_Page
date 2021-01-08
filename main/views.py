6d49d2c60694cfd361cd31efc9d9d84f932c2260
Jason Hubbard <jasonhubb@gmail.com>
2021-01-07 22:06:18 -0800

    creates HomePageView template

--- main/views.py
+++ main/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
+from django.views.generic.base import TemplateView
+from django.utils.decorators import method_decorator
+from main.decorators import unauthenticated_user
+
+
+
 from django.contrib.auth.views import (LoginView, LogoutView)
+
+
+class HomePageView(TemplateView):
+
+    template_name = "main/home.html"
+
+    @method_decorator(unauthenticated_user) #If user is already authenticated they will be redirected to their subscription page
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 class UserLoginView(LoginView):
 
