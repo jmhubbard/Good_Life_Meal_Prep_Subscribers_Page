86c003bc466ff4b465b3c44fa37d8d79cb4a70ad
Jason Hubbard <jasonhubb@gmail.com>
2021-01-07 22:07:31 -0800

    creates a path for subscription updates

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 
-from .views import subscriptionView
+from .views import subscriptionView, SubscriptionUpdate
 
 urlpatterns = [
-    path('', subscriptionView, name="subscriptions" )
+    path('', subscriptionView, name="subscriptions" ),
+    path('update/<int:pk>/', SubscriptionUpdate.as_view(), name='subscription-update')
 ]
