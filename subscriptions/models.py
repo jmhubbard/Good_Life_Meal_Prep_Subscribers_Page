4d291410a4d98dcb57c3e799e4ee2bb3cd7a9654
Jason Hubbard <jasonhubb@gmail.com>
2021-01-07 21:10:46 -0800

    creates SubscriptionItem model

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import User
+from recipes.models import Recipe
 
-# Create your models here.
+class SubscriptionItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subscription_item')
+    item = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='subscription_item')
+    quantity = models.PositiveSmallIntegerField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return 
