0e89634d164f7fc698630b5fe56a6b97bc5b87ba
Jason Hubbard <jasonhubb@gmail.com>
2021-01-08 01:01:31 -0800

    creates OrderItem model

--- orderitems/models.py
+++ orderitems/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import User
+from meals.models import Meal
 
-# Create your models here.
+class OrderItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='order_item')
+    item = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name='order_item')
+    quantity = models.PositiveSmallIntegerField(default=0)
+    special_requests = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user}'s order for {self.item}"
