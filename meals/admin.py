0a4285b42dd49a5ad16b6e15399931f8453c5730
Jason Hubbard <jasonhubb@gmail.com>
2021-01-08 00:05:25 -0800

    creates MealAdmin

--- meals/admin.py
+++ meals/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Meal
+
+class MealAdmin(admin.ModelAdmin):
+    list_display = ('is_active', 'name', 'description', 'proteins', 'carbs', 'fats', 'calories')
+    field_display = ('name','description','proteins', 'carbs', 'fats', 'calories', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+admin.site.register(Meal, MealAdmin)
