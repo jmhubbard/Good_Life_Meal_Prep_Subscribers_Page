b43298265cc72ca8c53232c3dd343071208e7515
Jason Hubbard <jasonhubb@gmail.com>
2021-01-07 21:37:07 -0800

    creates subscription view

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import SubscriptionItem
 
-# Create your views here.
+def subscriptionView(request):
+    current_user = request.user
+    subscriptions = SubscriptionItem.objects.filter(user = current_user, item__is_active = True).order_by('item__name')
+    context ={
+        'current_user': current_user,
+        'subscriptions': subscriptions
+    }
+
+    return render(request, 'subscriptions/subform.html', context)
