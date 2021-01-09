d25b6a0875581b539400599d205bd81384544aa8
Jason Hubbard <jasonhubb@gmail.com>
2021-01-09 00:05:41 -0800

    creates meal_list.py to contain list of meal dicts

---
+++ meals/meal_list/meal_list.py
@@ -0,0 +1,10 @@
+meal_list = [
+    {"name": "Grilled Greek Chicken Souvlaki", "description": "Grilled Greek Chicken Souvlaki with Sauteed Onions, Blistered Cherry Tomatoes, and Couscous Pilaf. Accompanied with a Tzatziki Sauce.", "proteins": 38, "carbs": 40, "fats": 11, "calories": 403, "picture_url": "https://static.wixstatic.com/media/5bd43b_75e0c16edf8e44b6a19a250dc5bdd388~mv2.jpg/v1/fill/w_75,h_75,usm_1.20_1.00_0.01/file.jpg", "is_active": False},
+    {"name": "Kalua Pork", "description": "Slow-Cooked Pork Shoulder with Wilted Cabbage. Served with Roasted Carrots, Spiced Cauliflower, and Calrose Rice.", "proteins": 37, "carbs": 44, "fats": 20, "calories": 513, "picture_url": "", "is_active": False},
+    {"name": "Shrimp and Sausage Creole Gumbo", "description": "Shrimp and Sausage Gumbo with Bell Peppers. Served with Calrose Rice and garnished with Scallions.", "proteins": 37, "carbs": 39, "fats": 6, "calories": 367, "picture_url": "", "is_active": False},
+    {"name": "Southwestern Turkey Burger", "description": "Southwestern (Ground) Turkey Burger (no bun) with Black Bean, Corn, and Bell Peppers. Served with Sweet Potato Fries. Accompanied with a Maple Mustard Sauce.", "proteins": 32, "carbs": 37, "fats": 10, "calories": 402, "picture_url": "https://static.wixstatic.com/media/5bd43b_75e0c16edf8e44b6a19a250dc5bdd388~mv2.jpg/v1/fill/w_466,h_350,usm_1.20_1.00_0.01/file.jpg", "is_active": False},
+]
+
+
+
+template = {"name": "", "description": "", "proteins": 0, "carbs": 0, "fats": 0, "calories": 0, "picture_url": "", "is_active": False},
