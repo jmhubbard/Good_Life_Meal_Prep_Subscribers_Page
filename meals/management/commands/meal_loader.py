e4193b5b51c147da55bf533850a87c3858505e30
Jason Hubbard <jasonhubb@gmail.com>
2021-01-09 00:06:07 -0800

    creates meal_loader which loads meals from meal_list

---
+++ meals/management/commands/meal_loader.py
@@ -0,0 +1,47 @@
+from django.core.management.base import BaseCommand
+from django.db import IntegrityError
+
+
+from meals.models import Meal
+from meals.meal_list.meal_list import meal_list
+
+
+class Command(BaseCommand):
+    help = 'Creates meals from meal_list'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--save', action='store_true', help='Save meals to the database')
+
+    def handle(self, *args, **options):
+        totalAttemptedItems = 0
+        #Meal Counts
+        savedMealCount = 0
+        duplicateMeals = 0
+
+        for item in meal_list:
+            totalAttemptedItems += 1
+            #Create an instance of Meal using the provided show name
+            meal = Meal(
+                name = item["name"],
+                description = item["description"],
+                proteins = item["proteins"],
+                carbs = item["carbs"],
+                fats = item["fats"],
+                calories = item["calories"],
+                picture_url = item["picture_url"],
+                is_active = item["is_active"],
+            )
+            if options["save"]:
+                    try:
+                        #Save the show instance if it doesn't exist in database
+                        meal.save()
+                    except IntegrityError:
+                        #If the show already exists then get that shows object to use when saving episodes
+                        duplicateMeals += 1
+                    else:
+                        savedMealCount += 1
+
+        print(f'Total attempted meals: {totalAttemptedItems}')
+        #Final Show Counts
+        print(f'Total saved meals: {savedMealCount}')
+        print(f'Skipped {duplicateMeals} duplicated meals')
