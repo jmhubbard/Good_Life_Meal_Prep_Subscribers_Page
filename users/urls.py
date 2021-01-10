89b0898d376ec9a788fa37d8437ada94ce2737be
Jason Hubbard <jasonhubb@gmail.com>
2021-01-09 20:58:15 -0800

    changes signup back to create so createview would work properly

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from .views import UserSignUpView, CustomPasswordChangeView, CustomPasswordChang
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name="login"),
     path('logout/' , UserLogoutView.as_view(), name='logout'),
-    path('signup/', UserSignUpView.as_view(), name="user_sign_up"),
+    path('create/', UserSignUpView.as_view(), name="user_sign_up"),
     path('change_password/', CustomPasswordChangeView.as_view(), name='password_change'),
     path('change_password_done/', CustomPasswordChangeDoneView.as_view(), name='password_change_done'),
 
