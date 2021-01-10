28a54f0879b0c1c637fc19587dcaab5593052ff6
Jason Hubbard <jasonhubb@gmail.com>
2021-01-09 19:32:12 -0800

    creates email_test function to test email

---
+++ orderitems/utils.py
@@ -0,0 +1,12 @@
+import os
+from django.core.mail import send_mail
+
+
+def email_test(user, message):
+    send_mail(
+        'Quote test',
+        message,
+        os.getenv("EMAIL_HOST_USER"),
+        [user],
+        fail_silently=False,
+    )
