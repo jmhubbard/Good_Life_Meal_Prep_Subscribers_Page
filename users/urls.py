9171fcd8ed0b3b4c293f1f1b2796c508eec36221
Jason Hubbard <jasonhubb@gmail.com>
2021-01-09 18:51:29 -0800

    adds path for create, password change and password change done

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 from main.views import UserLoginView, UserLogoutView
-
+from .views import UserCreateView, CustomPasswordChangeView, CustomPasswordChangeDoneView
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name="login"),
     path('logout/' , UserLogoutView.as_view(), name='logout'),
+    path('create/', UserCreateView.as_view(), name="user_create"),
+    path('change_password/', CustomPasswordChangeView.as_view(), name='password_change'),
+    path('change_password_done/', CustomPasswordChangeDoneView.as_view(), name='password_change_done'),
+
 ]
