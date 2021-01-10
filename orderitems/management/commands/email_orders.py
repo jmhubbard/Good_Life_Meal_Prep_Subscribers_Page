c9b03c0bd7774b834d4567fe352d54f230c28cbd
Jason Hubbard <jasonhubb@gmail.com>
2021-01-09 20:57:29 -0800

    creates email_orders command

--- orderitems/management/commands/email_orders.py
+++ orderitems/management/commands/email_orders.py
@@ -7,7 +7,7 @@ from django.core.mail import send_mail
 
 from meals.models import Meal
 from meals.meal_list.meal_list import meal_list
-from orderitems.utils import email_test
+from orderitems.utils import emailWeeklyOrders
 
 
 class Command(BaseCommand):
@@ -15,6 +15,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         user = "jasonhubb@gmail.com"
-        message = "Test"
 
-        email_test(user, message)
+        emailWeeklyOrders(user)
