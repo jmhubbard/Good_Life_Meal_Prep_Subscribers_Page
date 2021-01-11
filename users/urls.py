da0c67d2502377fdb9866604fd220ca65bbfced9
Jason Hubbard <jasonhubb@gmail.com>
2021-01-10 23:56:37 -0800

    creates url path for user profiles and delete account

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,8 @@ from main.views import UserLoginView, UserLogoutView
 from .views import (UserSignUpView, CustomPasswordChangeView,
                     CustomPasswordChangeDoneView, CustomPasswordResetView,
                     CustomPasswordResetDoneView, CustomPasswordResetConfirmView,
-                    CustomPasswordResetCompleteView) 
+                    CustomPasswordResetCompleteView,UserProfileUpdateView,
+                    UserDeleteView) 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name="login"),
     path('logout/' , UserLogoutView.as_view(), name='logout'),
@@ -15,5 +16,7 @@ urlpatterns = [
     path('reset_password_sent/', CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset_password_complete/', CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('profile/<int:pk>/', UserProfileUpdateView.as_view(), name="profile"),
+    path('delete_account/<int:pk>/', UserDeleteView.as_view(), name='delete_account'),
 
 ]
