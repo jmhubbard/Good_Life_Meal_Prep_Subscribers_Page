e7fc2901a28b6826d744ae34218df4e143958d45
Jason Hubbard <jasonhubb@gmail.com>
2021-01-10 19:36:43 -0800

    removes user and item from being read only fields

--- orderitems/admin.py
+++ orderitems/admin.py
@@ -6,7 +6,7 @@ from orderitems.models import OrderItem
 class OrderItemAdmin(admin.ModelAdmin):
     list_display = ('user', 'item', 'quantity', 'special_requests', 'is_on_current_menu', 'updated_at')
     field_display = ('user', 'item', 'quantity', 'special_requests', 'is_on_current_menu', 'updated_at')
-    readonly_fields = ('user', 'item', 'updated_at',)
+    readonly_fields = ('updated_at',)
     search_fields = ('user', 'item')
     ordering = ('user',)
 
