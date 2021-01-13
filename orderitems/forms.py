24a91e5f82b8c3fd52a6bbc97f7e805f736a42fc
Jason Hubbard <jasonhubb@gmail.com>
2021-01-13 14:11:30 -0800

    add quantity validation to keep quantity less than 50

--- orderitems/forms.py
+++ orderitems/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from .models import OrderItem
 
+from django.core.exceptions import ValidationError
+
+
 class OrderItemUpdateForm(forms.ModelForm):
     class Meta:
         model = OrderItem
@@ -10,3 +13,11 @@ class OrderItemUpdateForm(forms.ModelForm):
             'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
             'special_requests': forms.Textarea(attrs={'class': 'form-control'}),
         }
+
+    def clean_quantity(self):
+        quantity = self.cleaned_data['quantity']
+        if quantity > 50:
+            raise forms.ValidationError("Quantity must be less than 50")
+        else:
+            return quantity
+
