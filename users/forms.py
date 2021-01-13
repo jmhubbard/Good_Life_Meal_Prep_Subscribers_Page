8f05a9040de47beaf8e63417ca964e1a37b4bb78
Jason Hubbard <jasonhubb@gmail.com>
2021-01-12 20:41:53 -0800

    creates userupdateform, customusersetpasswordform and customuserpasswordchangeform

--- users/forms.py
+++ users/forms.py
@@ -3,18 +3,33 @@ from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.password_validation import validate_password
 
+from django.contrib.auth.forms import SetPasswordForm
+
 from .models import User
 from meals.models import Meal
 from orderitems.models import OrderItem
 
+from django.utils.translation import gettext_lazy as _
+
+from django.contrib.auth import password_validation
+from django.core.exceptions import ValidationError
+
+
+
+
 class UserSignUpForm(forms.ModelForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
-    date_of_birth = forms.DateField(widget=forms.SelectDateWidget)
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
     class Meta:
         model = User
         fields = ('email','name', 'date_of_birth')
+
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'date_of_birth': forms.SelectDateWidget(attrs={'class': 'form-control'})
+        }
 
     def clean_email(self):
         email = self.cleaned_data['email']
@@ -35,3 +50,89 @@ class UserSignUpForm(forms.ModelForm):
         user.set_password(self.cleaned_data["password2"])
         user.save()
         return user
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('name', 'date_of_birth')
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'date_of_birth': forms.SelectDateWidget(attrs={'class': 'form-control'}),
+        }
+
+
+#Copied from SetPasswordForm
+class CustomUserSetPasswordForm(forms.Form):
+    """
+    A form that lets a user change set their password without entering the old
+    password
+    """
+    error_messages = {
+        'password_mismatch': _('The two password fields didn’t match.'),
+    }
+    new_password1 = forms.CharField(
+        label=_("New password"),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'form-control'}),
+        strip=False,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    new_password2 = forms.CharField(
+        label=_("New password confirmation"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'form-control'}),
+    )
+
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super().__init__(*args, **kwargs)
+
+    def clean_new_password2(self):
+        password1 = self.cleaned_data.get('new_password1')
+        password2 = self.cleaned_data.get('new_password2')
+        if password1 and password2:
+            if password1 != password2:
+                raise ValidationError(
+                    self.error_messages['password_mismatch'],
+                    code='password_mismatch',
+                )
+        password_validation.validate_password(password2, self.user)
+        return password2
+
+    def save(self, commit=True):
+        password = self.cleaned_data["new_password1"]
+        self.user.set_password(password)
+        if commit:
+            self.user.save()
+        return self.user
+
+#Copied from PasswordChangeForm
+class CustomUserPasswordChangeForm(CustomUserSetPasswordForm):
+    """
+    A form that lets a user change their password by entering their old
+    password.
+    """
+    error_messages = {
+        **SetPasswordForm.error_messages,
+        'password_incorrect': _("Your old password was entered incorrectly. Please enter it again."),
+    }
+    old_password = forms.CharField(
+        label=_("Old password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'autofocus': True, 'class':'form-control'}),
+    )
+
+    field_order = ['old_password', 'new_password1', 'new_password2']
+
+    def clean_old_password(self):
+        """
+        Validate that the old_password field is correct.
+        """
+        old_password = self.cleaned_data["old_password"]
+        if not self.user.check_password(old_password):
+            raise ValidationError(
+                self.error_messages['password_incorrect'],
+                code='password_incorrect',
+            )
+        return old_password
