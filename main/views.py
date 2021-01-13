505fde11107acf85e6668a949a257a99b1e4c60a
Jason Hubbard <jasonhubb@gmail.com>
2021-01-13 14:55:46 -0800

    adds docstring comments to HomePageView, UserLoginView, and UserLogoutView

--- main/views.py
+++ main/views.py
@@ -10,6 +10,9 @@ from django.contrib.auth.views import (LoginView, LogoutView)
 
 
 class HomePageView(TemplateView):
+    """
+    The homepage view that just include a signup button and some explanitory text.
+    """
 
     template_name = "main/home.html"
 
@@ -18,6 +21,10 @@ class HomePageView(TemplateView):
         return super().dispatch(*args, **kwargs)
 
 class UserLoginView(LoginView):
+    """
+    The login view for unauthenticated users. If a user is already authenticated,
+    they will be redirected to the menu page.
+    """
 
     form_class = CustomAuthenticationForm
     template_name = "registration/login.html"
@@ -27,5 +34,8 @@ class UserLoginView(LoginView):
         return super().dispatch(*args, **kwargs)
 
 class UserLogoutView(LogoutView):
+    """
+    A generic logout view that redirects users to the homepage after logging them out.
+    """
 
     next_page = 'home'
