e6285bbaa8bca540b06402e38106cd0a75187351
Jason Hubbard <jasonhubb@gmail.com>
2021-01-13 14:45:22 -0800

    adds docstring comments to create_order_items_for_new_user and create_order_items_for_new_meal

--- orderitems/signals.py
+++ orderitems/signals.py
@@ -1,11 +1,16 @@
 from django.db.models.signals import post_save
 
-
-from users.models import User
-from meals.models import Meal
 from .models import OrderItem
+from meals.models import Meal
+from users.models import User
+
 
 def create_order_items_for_new_user(sender, instance, created, **kwargs):
+    """
+    A function that creates OrderItems for every Meal in the database for a newly created User. If the meal is currently on the menu, the
+    Orderitem.is_on_current_menu will be set to True. Otherwise it's set to False. This allows users
+    who sign 
+    """
 
     if created:
         all_current_meals = Meal.objects.filter(is_on_menu=True)
@@ -18,6 +23,10 @@ def create_order_items_for_new_user(sender, instance, created, **kwargs):
 post_save.connect(create_order_items_for_new_user, sender=User)
 
 def create_order_items_for_new_meal(sender, instance, created, **kwargs):
+    """
+    A function that creates a new OrderItem for every user currently in the database, when a new meal
+    is created.
+    """
 
     if created:
         all_users = User.objects.all()
