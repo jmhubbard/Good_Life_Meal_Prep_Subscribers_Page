7ab6d5e5abf5b89cf916c608c8c216088c1d7ac7
Jason Hubbard <jasonhubb@gmail.com>
2021-01-17 11:37:31 -0800

    adds static_root

--- project/settings.py
+++ project/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'users.User'
