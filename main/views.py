b4f4d1d24122c1076575a2d4d3629500ae4a861b
Jason Hubbard <jasonhubb@gmail.com>
2021-01-18 10:40:35 -0800

    Creates ContactFormView

--- main/views.py
+++ main/views.py
@@ -4,9 +4,21 @@ from django.views.generic.base import TemplateView
 from django.utils.decorators import method_decorator
 from .decorators import unauthenticated_user
 
-from .forms import CustomAuthenticationForm
+from .forms import CustomAuthenticationForm, ContactForm
 
 from django.contrib.auth.views import (LoginView, LogoutView)
+
+from django.contrib.messages.views import SuccessMessageMixin
+
+from django.views.generic.edit import FormView
+
+from django.urls import reverse_lazy
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from users.models import User
+
+
 
 
 class HomePageView(TemplateView):
@@ -39,3 +51,17 @@ class UserLogoutView(LogoutView):
     """
 
     next_page = 'home'
+
+
+class ContactFormView(SuccessMessageMixin, LoginRequiredMixin, FormView):
+    template_name = "main/contact_form.html"
+    form_class = ContactForm
+    success_url = reverse_lazy('contact_form')
+    success_message = "Thank you for contacting us. We value your feedback. Somebody will reply within 24 hours."
+
+    def form_valid(self, form):
+        current_user = User.objects.get(email=self.request.user)
+        form.send_message(current_user)
+        return super().form_valid(form)
+
+
