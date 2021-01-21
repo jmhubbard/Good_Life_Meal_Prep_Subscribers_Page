d79b11651803dea3c5648a94c95fea25ed82b739
Jason Hubbard <jasonhubb@gmail.com>
2021-01-21 13:59:45 -0800

    adds day of the week check to only run on fridays

--- orderitems/management/commands/email_orders.py
+++ orderitems/management/commands/email_orders.py
@@ -9,10 +9,17 @@ from meals.models import Meal
 from meals.meal_list.meal_list import meal_list
 from orderitems.utils import emailWeeklyOrders
 
+import datetime
+
 
 class Command(BaseCommand):
     help = 'Emails admin an email containing the weekly orders'
 
     def handle(self, *args, **options):
+        current_day_of_the_week = datetime.datetime.today().weekday()
 
-        emailWeeklyOrders()
+        if current_day_of_the_week == 4:
+            emailWeeklyOrders()
+            print("Orders have been emailed")
+        else:
+            print("Today is not Friday")
