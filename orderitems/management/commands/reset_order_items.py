9be8f3a0706e2ca1a7b4ab6b01daf4cbbfa76074
Jason Hubbard <jasonhubb@gmail.com>
2021-01-21 15:57:29 -0800

    adds a check to only run code on Sundays

--- orderitems/management/commands/reset_order_items.py
+++ orderitems/management/commands/reset_order_items.py
@@ -4,13 +4,20 @@ from django.db import IntegrityError
 
 from orderitems.models import OrderItem
 
+import datetime
+
 
 class Command(BaseCommand):
     help = 'Resets all previous weeks order items quantity to 0 and is_on_current_menu to False'
 
     def handle(self, *args, **options):
-        current_menu_order_items = OrderItem.objects.filter(is_on_current_menu=True)
-        for item in current_menu_order_items:
-            item.quantity = 0
-            item.is_on_current_menu = False
-            item.save()
+        current_day_of_the_week = datetime.datetime.today().weekday()
+
+        if current_day_of_the_week == 6:
+            current_menu_order_items = OrderItem.objects.filter(is_on_current_menu=True)
+            for item in current_menu_order_items:
+                item.quantity = 0
+                item.is_on_current_menu = False
+                item.save()
+        else:
+            print("Today isn't Sunday so items won't be reset")
--- orderitems/management/commands/update_remaining_meals.py
+++ orderitems/management/commands/update_remaining_meals.py
@@ -4,23 +4,28 @@ from django.db import IntegrityError
 from orderitems.models import OrderItem
 from users.models import User
 
+import datetime
 
 class Command(BaseCommand):
     help = 'Updates all users remaining meal totals after the current weeks order'
 
     def handle(self, *args, **options):
-        all_users = User.objects.filter(is_active=True)
+        current_day_of_the_week = datetime.datetime.today().weekday()
 
-        for user in all_users:
-            number_of_meals_ordered = 0
-            weekly_order = OrderItem.objects.filter(user=user, is_on_current_menu=True)
-            for item in weekly_order:
-                number_of_meals_ordered += item.quantity
-            user.remaining_meals -= number_of_meals_ordered
-            try:
-                user.save()
-            except IntegrityError:
-                user.remaining_meals = 0
-                user.save()
-                print("User went over meals")
-        
+        if current_day_of_the_week == 6:
+            all_users = User.objects.filter(is_active=True)
+
+            for user in all_users:
+                number_of_meals_ordered = 0
+                weekly_order = OrderItem.objects.filter(user=user, is_on_current_menu=True)
+                for item in weekly_order:
+                    number_of_meals_ordered += item.quantity
+                user.remaining_meals -= number_of_meals_ordered
+                try:
+                    user.save()
+                except IntegrityError:
+                    user.remaining_meals = 0
+                    user.save()
+                    print("User went over meals")
+        else:
+            print("Today isn't Sunday so remaining meals won't be calculated")
