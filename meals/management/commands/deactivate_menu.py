f497f5fa3cfd4cc0389484f035eab995b0fac0d5
Jason Hubbard <jasonhubb@gmail.com>
2021-01-21 13:37:10 -0800

    changes command to check if the day is friday before running so that we can use free heorku scheduler

--- meals/management/commands/deactivate_menu.py
+++ meals/management/commands/deactivate_menu.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.db import IntegrityError
+import datetime
 
 
 from meals.models import Meal
@@ -9,7 +10,13 @@ class Command(BaseCommand):
     help = 'Deactivates the current menu so users cannot update their orders'
 
     def handle(self, *args, **options):
-        current_menu = Meal.objects.filter(is_on_menu=True)
-        for item in current_menu:
-            item.is_on_menu = False
-            item.save()
+        current_day_of_the_week = datetime.datetime.today().weekday()
+
+        if current_day_of_the_week == 4:
+            current_menu = Meal.objects.filter(is_on_menu=True)
+            for item in current_menu:
+                item.is_on_menu = False
+                item.save()
+            print("Menu has been deactivated")
+        else:
+            print("Menu is still active")
