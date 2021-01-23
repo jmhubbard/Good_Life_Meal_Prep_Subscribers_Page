1ca6185ac7cce0a9b7690abe8898d37363d8ffd5
Jason Hubbard <jasonhubb@gmail.com>
2021-01-22 16:36:34 -0800

    adds user login link to emails

--- users/utils.py
+++ users/utils.py
@@ -10,14 +10,18 @@ from .models import User
 def email_admin_new_user_sign_up(new_user):
     
     domain = Site.objects.get_current().domain
-    path = reverse('admin:index')
-    url = f'http://{domain}{path}'
+    admin_path = reverse('admin:index')
+    admin_url = f'http://{domain}{admin_path}'
+
+    user_login_path = reverse('login')
+    user_url = f'http://{domain}{user_login_path}'
     
     recipient_list = User.objects.filter(is_admin=True)
     
     context = {
         "new_user": new_user,
-        "admin_link": url,
+        "admin_link": admin_url,
+        "user_url": user_url
     }
 
     message_text = render_to_string("users/email_admin_new_user_signup.txt", context=context)
