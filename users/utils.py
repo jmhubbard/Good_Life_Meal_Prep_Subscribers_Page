85f4742b6b04923bdfade9df90fd7e917c652872
Jason Hubbard <jasonhubb@gmail.com>
2021-01-28 23:35:37 -0800

    cleans up page

--- users/utils.py
+++ users/utils.py
@@ -1,46 +1,40 @@
-import os
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
-from django.urls import reverse
-from django.contrib.sites.models import Site
-
 from .models import User
-
-
-def email_admin_new_user_sign_up(new_user):
-    
-    domain = Site.objects.get_current().domain
-    admin_path = reverse('admin:index')
-    admin_url = f'http://{domain}{admin_path}'
-
-    user_login_path = reverse('login')
-    user_url = f'http://{domain}{user_login_path}'
-    
-    recipient_list = User.objects.filter(is_admin=True)
-    
-    context = {
-        "new_user": new_user,
-        "admin_link": admin_url,
-        "user_url": user_url
-    }
-
-    message_text = render_to_string("users/email_admin_new_user_signup.txt", context=context)
-    message_html = render_to_string("users/email_admin_new_user_signup.html", context=context)
-    return send_mail(
-        'The Good Life Meal Prep Subscribers New User Signup Notification',
-        message_text,
-        os.getenv("EMAIL_HOST_USER"),
-        recipient_list,
-        fail_silently=False,
-        html_message=message_html,
-    )
+from main.utils import get_admin_login_url, get_login_url, send_email
 
 
 def get_all_active_users():
+    """
+    Returns a queryset of all active users.
+    """
     all_users = User.objects.filter(is_active=True)
     return all_users
 
+
 def get_all_admins():
-    current_admins = User.objects.filter(is_admin=True)
+    """
+    Returns a queryset of all active admin users.
+    """
+    current_admins = User.objects.filter(is_admin=True, is_active=True)
     return current_admins
+
+
+def email_admin_new_user_sign_up(new_user):
+    """
+    Sends an email to all the admins that a new user has signed up on the platform.
+    """
+    admin_login_url = get_admin_login_url()
+    user_login_url = get_login_url()
+
+    context = {
+        "new_user": new_user,
+        "admin_login_url": admin_login_url,
+        "user_login_url": user_login_url,
+    }
+    message_text = "users/email_admin_new_user_signup.txt"
+    message_html = "users/email_admin_new_user_signup.html"
+    subject = "The Good Life Meal Prep Subscribers New User Signup Notification"
+    current_admins = get_all_admins()
+    current_admins_emails = [admin.email for admin in current_admins]
+
+    send_email(context=context, message_text=message_text, message_html=message_html, subject=subject, recipient_list=current_admins_emails)
 
