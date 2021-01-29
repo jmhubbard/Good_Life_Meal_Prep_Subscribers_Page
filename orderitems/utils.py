435f4733c063880f98591f03ae049e660b529ea6
Jason Hubbard <jasonhubb@gmail.com>
2021-01-28 22:08:37 -0800

    creates get_users_order and get_all_users_order functions. Refactors emailWeeklyOrder and weekly_reminder_email to use new functions

--- orderitems/utils.py
+++ orderitems/utils.py
@@ -1,78 +1,90 @@
-import os
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
-from django.urls import reverse
-
-from django.contrib.sites.models import Site
 import datetime
+import os
 
-
-
-from users.models import User
+from main.utils import get_login_url, send_email
 from orderitems.models import OrderItem
+from users.utils import get_all_active_users, get_all_admins
+
+
+def get_users_order(user):
+    """
+    Returns a queryset of a specific users current order items that have a quantity greater than 0
+    """
+    users_order_items = OrderItem.objects.filter(user=user, is_on_current_menu=True, quantity__gt=0)
+    return users_order_items
+
+
+def get_all_users_orders():
+    """
+    Returns a list containing a dictionary for each active user.
+    
+    Each dictionary contains the customers name, email, phone_number
+    street_address, city, state, zipcode, and a list of all their meals.
+    """
+    all_customers = get_all_active_users()
+    all_orders = []
+
+    for customer in all_customers:
+        users_order_items = get_users_order(customer)
+        order = {"name": customer.name, "email": customer.email, "phone_number": customer.phone_number.as_national, "street_address": customer.street_address, "city": customer.city, "state": customer.state, "zipcode": customer.zip_code, "meals": users_order_items}
+        all_orders.append(order)
+
+    return all_orders
+
 
 def emailWeeklyOrders():
-    all_users = User.objects.filter(is_active=True)
-    orders = []
-
-    for customer in all_users:
-        orderItems = OrderItem.objects.filter(user=customer, is_on_current_menu=True, quantity__gt=0)
-
-        order = {"name": customer.name, "email": customer.email, "phone_number": customer.phone_number.as_national, "street_address": customer.street_address, "city": customer.city, "state": customer.state, "zipcode": customer.zip_code, "meals": orderItems}
-        orders.append(order)
-
-    domain = Site.objects.get_current().domain
-
-    user_login_path = reverse('login')
-    user_url = f'http://{domain}{user_login_path}'
-
-    current_date = datetime.date.today()
+    """
+    Emails all the current admins an email containing every users current order.
     
+    Returns total emails sent and the email for every admin.
+    """
+    all_orders = get_all_users_orders()
+    user_login_url = get_login_url()
+    current_date = datetime.date.today()
     context = {
-        "orders": orders,
-        "user_url": user_url,
+        "all_orders": all_orders,
+        "user_login_url": user_login_url,
         "current_date": current_date,
-
     }
-    message_text = render_to_string("orderitems/email_weekly_orders.txt", context=context)
-    message_html = render_to_string("orderitems/email_weekly_orders.html", context=context)
 
+    subject = 'Weekly Orders Email'
+    message_text = "orderitems/email_weekly_orders.txt"
+    message_html = "orderitems/email_weekly_orders.html"
+    current_admins = get_all_admins()
+    current_admins_emails = [admin.email for admin in current_admins]
 
-    current_admins = User.objects.filter(is_admin=True)
+    total_emails_sent = send_email(context=context, message_text=message_text, message_html=message_html, subject=subject, recipient_list=current_admins_emails)
+    
+    return total_emails_sent, current_admins_emails
 
-    send_mail(
-        'Weekly Orders Email',
-        message_text,
-        os.getenv("EMAIL_HOST_USER"),
-        current_admins,
-        fail_silently=False,
-        html_message=message_html,
-    )
 
 def weekly_reminder_email():
-    all_users = User.objects.filter(is_active=True)
-    domain = Site.objects.get_current().domain
-    user_login_path = reverse('login')
-    user_url = f'http://{domain}{user_login_path}'
+    """
+    Emails every active user a reminder to place their orders by Thursday at 11:59 pm.
+    
+    If the user has already placed an order, the email will contain what their current
+    order is and a reminder that they can edit their order before 11:59 pm.
+
+    Returns total emails sent.
+    """
+    all_users = get_all_active_users()
+    user_login_url = get_login_url()
+    subject = "Don't forget to place your orders!"
+    message_text = "orderitems/weekly_reminder_email.txt"
+    message_html = "orderitems/weekly_reminder_email.html"
+
+    total_emails_sent = 0
 
     for customer in all_users:
-        orderItems = OrderItem.objects.filter(user=customer, is_on_current_menu=True, quantity__gt=0)
-        print(customer)
-
+        users_order_items = get_users_order(customer)
         context = {
-            "user_url": user_url,
-            "orderItems": orderItems,
+            "user_login_url": user_login_url,
+            "users_order_items": users_order_items,
             "customer": customer,
         }
+        
+        recipient_list = [customer.email]
 
-        message_text = render_to_string("orderitems/weekly_reminder_email.txt", context=context)
-        message_html = render_to_string("orderitems/weekly_reminder_email.html", context=context)
-
-        send_mail(
-            "Don't forget to place your orders!",
-            message_text,
-            os.getenv("EMAIL_HOST_USER"),
-            [customer.email,],
-            fail_silently=False,
-            html_message=message_html,
-        )
+        total_emails_sent += send_email(context, message_text, message_html, subject, recipient_list)
+    
+    return total_emails_sent
