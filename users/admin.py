422d980959c680fbec769a52b221866363f42064
Jason Hubbard <jasonhubb@gmail.com>
2021-02-04 22:10:46 -0800

    formatted all the imports

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,9 @@
 from django import forms
-from django.contrib import admin
-from django.contrib.auth.models import Group
+from django.contrib import admin, messages
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth.models import Group
 from django.core.exceptions import ValidationError
-
-from django.contrib import messages
 from django.utils.translation import ngettext
 
 from .models import User
