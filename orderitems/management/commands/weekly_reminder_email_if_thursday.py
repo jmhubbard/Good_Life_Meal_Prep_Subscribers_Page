ff4ab48852892dbc3604b03ed6c971ebcd6bc414
Jason Hubbard <jasonhubb@gmail.com>
2021-02-11 22:00:41 -0800

    deletes weekly_reminder_any_day and adds optional argument to weekly_reminder_email_if_thursday to allow for email to be sent any day

--- orderitems/management/commands/weekly_reminder_email_if_thursday.py
+++ orderitems/management/commands/weekly_reminder_email_if_thursday.py
@@ -9,8 +9,16 @@ import datetime
 class Command(BaseCommand):
     help = 'Emails admin an email containing the weekly orders'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--any_day', action='store_true', help='Sends email regardless of the day of the week')
+
+
     def handle(self, *args, **options):
         current_day_of_the_week = datetime.datetime.today().weekday()
+
+        if options['any_day']:
+            current_day_of_the_week = 3
+
 
         if current_day_of_the_week == 3:
             weekly_reminder_email()
