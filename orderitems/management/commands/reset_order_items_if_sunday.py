05b63b12176c6dc0ae3666bcf40f5ff87ec5a162
Jason Hubbard <jasonhubb@gmail.com>
2021-02-11 22:11:24 -0800

    deletes reset_order_items_any_day and adds an optional argument to reset_order_items_if_sunday to allow for any day

--- orderitems/management/commands/reset_order_items_if_sunday.py
+++ orderitems/management/commands/reset_order_items_if_sunday.py
@@ -10,8 +10,16 @@ import datetime
 class Command(BaseCommand):
     help = 'Resets all previous weeks order items quantity to 0 and is_on_current_menu to False'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--any_day', action='store_true', help='Runs the function regardless of the day of the week.')
+
+
     def handle(self, *args, **options):
         current_day_of_the_week = datetime.datetime.today().weekday()
+
+        if options['any_day']:
+            current_day_of_the_week = 6
+
 
         if current_day_of_the_week == 6:
             current_menu_order_items = OrderItem.objects.filter(is_on_current_menu=True)
@@ -19,5 +27,6 @@ class Command(BaseCommand):
                 item.quantity = 0
                 item.is_on_current_menu = False
                 item.save()
+            print("Item order quantities have been set to 0")
         else:
             print("Today isn't Sunday so items won't be reset")
