c1908a24cdc16fd019142a6b388856bbd8869efa
Jason Hubbard <jasonhubb@gmail.com>
2021-02-11 22:32:25 -0800

    deletes deactivate_menu_any_day and adds an optional argument to deactivate_menu_if_friday to allow for any day

--- meals/management/commands/deactivate_menu_if_friday.py
+++ meals/management/commands/deactivate_menu_if_friday.py
@@ -9,8 +9,15 @@ from meals.models import Meal
 class Command(BaseCommand):
     help = 'Deactivates the current menu so users cannot update their orders.'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--any_day', action='store_true', help='Deactivates the menu regardless of the day of the week')
+
+
     def handle(self, *args, **options):
         current_day_of_the_week = datetime.datetime.today().weekday()
+
+        if options['any_day']:
+            current_day_of_the_week = 4
 
         if current_day_of_the_week == 4:
             all_meals = Meal.objects.all()
