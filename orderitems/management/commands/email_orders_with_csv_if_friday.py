1d1f1fd75b36064f0a778f6657cc345c47c4d6fc
Jason Hubbard <jasonhubb@gmail.com>
2021-02-11 21:55:37 -0800

    deletes email_orders_any_day and adds an optional argument to email_orders_with_csv to allow emails to be sent any day

--- orderitems/management/commands/email_orders_with_csv_if_friday.py
+++ orderitems/management/commands/email_orders_with_csv_if_friday.py
@@ -2,20 +2,34 @@ import datetime
 
 from django.core.management.base import BaseCommand
 
-from orderitems.utils import create_weekly_order_csv, email_weekly_orders_csv, weekly_order_confirmation_email
+from orderitems.utils import (create_weekly_order_csv, email_weekly_orders_csv,
+                                get_meal_totals_for_active_users, weekly_order_confirmation_email)
+from users.utils import get_all_active_users
 
 
 class Command(BaseCommand):
     help = 'Emails admin an email containing the weekly orders and sends all active users an order confirmation'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--any_day', action='store_true', help='Sends email regardless of the day of the week')
+
     def handle(self, *args, **options):
         current_day_of_the_week = datetime.datetime.today().weekday()
 
+        if options['any_day']:
+            current_day_of_the_week = 4
+
         if current_day_of_the_week == 4:
-            create_weekly_order_csv()
-            total_emails_sent, current_admins_emails = email_weekly_orders_csv()
+            all_active_users = get_all_active_users()
+            current_menu_meal_totals = get_meal_totals_for_active_users()
+
+            create_weekly_order_csv(all_active_users= all_active_users,
+                                    current_menu_meal_totals = current_menu_meal_totals
+                                    )
+
+            total_emails_sent, current_admins_emails = email_weekly_orders_csv(current_menu_meal_totals=current_menu_meal_totals)
             print("{} order emails have been sent to this list of admins: {}".format(total_emails_sent, current_admins_emails))
-            
+
             total_user_emails_sent, total_full_orders, total_empty_orders = weekly_order_confirmation_email()
             print("{} total order confirmations have been sent".format(total_user_emails_sent))
             print("{} users placed an order".format(total_full_orders))
