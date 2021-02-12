3482fc659ed42aa1be4289f64138374ab0b9f52c
Jason Hubbard <jasonhubb@gmail.com>
2021-02-11 23:53:24 -0800

    adds list_filter to orderItemAdmin

--- orderitems/admin.py
+++ orderitems/admin.py
@@ -5,6 +5,7 @@ from orderitems.models import OrderItem
 
 class OrderItemAdmin(admin.ModelAdmin):
     list_display = ('user', 'item', 'quantity', 'special_requests', 'is_on_current_menu', 'updated_at')
+    list_filter = ('is_on_current_menu',)
     field_display = ('user', 'item', 'quantity', 'special_requests', 'is_on_current_menu', 'updated_at')
     readonly_fields = ('updated_at',)
     search_fields = ('user__name', 'item__name')
