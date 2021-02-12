39f4e9de4f114879084d56e5471479550b958fbe
Jason Hubbard <jasonhubb@gmail.com>
2021-02-11 23:44:57 -0800

    removes related_names from user and item

--- orderitems/models.py
+++ orderitems/models.py
@@ -3,8 +3,8 @@ from users.models import User
 from meals.models import Meal
 
 class OrderItem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='order_item')
-    item = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name='order_item')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Meal, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField(default=0)
     special_requests = models.TextField(blank=True)
     is_on_current_menu = models.BooleanField(default=False)
